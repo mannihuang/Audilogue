39851e30e9256c9459a23e50d75d185c5b29b261
PGO\MHuang <manni.huang@wilcom.com>
2023-04-11 03:34:39 +1000

    Download all

--- src/components/pageComponents/workspace/DownloadAll.svelte
+++ src/components/pageComponents/workspace/DownloadAll.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
   import Button from '@smui/button/src/Button.svelte';
+  import JSZip from 'jszip';
+  import { speeches } from '../../../store/speeches';
+  import { allVoices } from '../../../store/user';
+  import { trimText } from '$lib/utils/text';
+
+  async function downloadAll() {
+    const zip = new JSZip();
+    let counter = 1;
+    for (const speech of $speeches) {
+      if (!speech.currentGeneration) {
+        continue;
+      }
+      const speechSelectedGen = speech.generationHistory.find(
+        (gen) => gen.id == speech.currentGeneration?.id
+      );
+      console.log({ speechSelectedGen });
+      const blob = await (await fetch(speechSelectedGen!.audioUri)).blob();
+      console.log({ blob });
+      const textFileNameBit = trimText(speech.text);
+      const voiceName = $allVoices.find((vc) => vc.voice_id == speech.voiceId)?.name;
+      zip.file(`${counter}. ${voiceName} - ${textFileNameBit}.mp3`, blob, { base64: true });
+      counter++;
+    }
+
+    const zipFolderData = await zip.generateAsync({ type: 'blob' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(zipFolderData);
+    link.download = 'audilogue_project';
+    link.click();
+  }
 </script>
 
 <div>
-  <Button variant="unelevated">Download all</Button>
+  <Button variant="unelevated" on:click={downloadAll}>Download all</Button>
 </div>
